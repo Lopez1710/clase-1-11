c849b44ed656ad9e1ee8f6839d74ccb370cd2aaf
Lopez1710 <69539735+Lopez1710@users.noreply.github.com>
2022-11-01 10:49:40 -0600

    lo que hice hoy

---
+++ OtroProyecto/app/src/main/java/com/example/otroproyecto/vista_fragmento.kt
@@ -0,0 +1,31 @@
+package com.example.otroproyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class vista_fragmento : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vista_fragmento, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.takeIf { it.containsKey(ARO_OBJECT)}?.apply {
+            val tv_mensaje:TextView = view.findViewById(R.id.tv_mensaje)
+            tv_mensaje.text = "vista fragment" + getInt(ARO_OBJECT).toString()
+        } }
+    companion object {
+        private const val ARO_OBJECT ="object"
+
+    }
+}
